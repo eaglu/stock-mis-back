b164c3201daf157d607493878413f64a9b9e6ca1
eaglu <chcw38@gmail.com>
2022-06-20 16:29:25 +0800

    Refactor repo

--- src/main/java/com/qfedu/mapper/RepoMapper.java
+++ src/main/java/com/qfedu/mapper/RepoMapper.java
@@ -1,12 +1,58 @@
 package com.qfedu.mapper;
 
-
 import com.qfedu.entity.Repo;
-import org.apache.ibatis.annotations.Select;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
-public interface RepoMapper extends Mapper<Repo>, MySqlMapper<Repo> {
+public interface RepoMapper {
+    @Insert("INSERT into repo (name,inventory,delete_flag) " +
+            "VALUES(#{name},#{inventory},#{deleteFlag})")
+    Integer insertRepo(Repo repo);
+
+    @Update("UPDATE repo SET name =#{name}" +
+            ",inventory=#{inventory} WHERE  id=#{id}")
+    Integer updateRepo(Repo repo);
+
+    @Update("UPDATE repo SET delete_flag=1 " +
+            "WHERE id=#{id}")
+    Integer deleteRepo(Integer id);
+
+
+
+    @Select("SELECT * FROM repo WHERE id=#{id}")
+    @Results({
+            @Result(id = true, column = "id", property = "id"),
+            @Result(column = "name", property = "name"),
+            @Result(column ="inventory",property = "inventory"),
+            @Result(column = "delete_flag", property = "deleteFlag")
+    })
+    Repo findRepoById(Repo repo);
+
+    @Select("SELECT * FROM repository WHERE name=#{name}")
+    @Results({
+            @Result(id = true, column = "id", property = "id"),
+            @Result(column = "name", property = "name"),
+            @Result(column ="inventory",property = "inventory"),
+            @Result(column = "delete_flag", property = "deleteFlag")
+    })
+    List<Repo> findRepoByName(Repo repo);
+
+    @Select("SELECT * FROM repo WHERE name like CONCAT('%',#{name},'%')")
+    @Results({
+            @Result(id = true, column = "id", property = "id"),
+            @Result(column = "name", property = "name"),
+            @Result(column ="inventory",property = "inventory"),
+            @Result(column = "delete_flag", property = "deleteFlag")
+    })
+    List<Repo> findRepoLikeName(Repo repo);
+
+    @Select("SELECT * FROM repo")
+    @Results({
+            @Result(id = true, column = "id", property = "id"),
+            @Result(column = "name", property = "name"),
+            @Result(column ="inventory",property = "inventory"),
+            @Result(column = "delete_flag", property = "deleteFlag")
+    })
+    List<Repo> findRepoAll();
 }
--- src/main/java/com/qfedu/service/impl/RepoServiceImpl.java
+++ src/main/java/com/qfedu/service/impl/RepoServiceImpl.java
@@ -1,43 +1,66 @@
 package com.qfedu.service.impl;
 
 import com.qfedu.entity.Repo;
-import com.qfedu.entity.User;
+import com.qfedu.entity.Repo;
 import com.qfedu.mapper.RepoMapper;
+import com.qfedu.mapper.RepoMapper;
+import com.qfedu.service.RepoService;
 import com.qfedu.service.RepoService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class RepoServiceImpl implements RepoService {
-
     @Autowired
     private RepoMapper repoMapper;
 
     @Override
-    public List<Repo> list(Repo quertCondition) {
-//json传过来的空字符串会导致tkmybatis查询时携带值为空的查询条件
-        if (StringUtils.isBlank(quertCondition.getName())) {
-            quertCondition.setName(null);
-        }
-        List list = repoMapper.select(quertCondition);
-        return list;
-    }
-
-    @Override
-    public Integer add(Repo entity) {
-        entity.setOperatorId(1);
-        entity.setOperatorName("千锋虎哥");
-        entity.setOperatorTime(new Date());
-        Integer rsCount = repoMapper.insert(entity);
+    public Integer updateRepo(Repo repo) {
+        Integer rsCount= repoMapper.updateRepo(repo);
         return rsCount;
     }
 
     @Override
-    public List<Repo> repos() {
-        return repoMapper.selectAll();
+    public Integer deleteRepo(Integer id) {
+        Integer rsCount = repoMapper.deleteRepo(id);
+        return rsCount;
+    }
+
+
+    @Override
+    public Repo findRepoById(Repo repo) {
+        return repoMapper.findRepoById(repo);
+    }
+
+
+    @Override
+    public Integer insertGoods(Repo repo) {
+        Integer rsCount = 0;
+        List<Repo> list = repoMapper.findRepoByName(repo);
+        list.removeIf(goods -> goods.getDeleteFlag() == 1);
+        if (list.size() == 0) {
+            rsCount = repoMapper.insertRepo(repo);
+        }
+        return rsCount;
+    }
+
+    @Override
+    public List<Repo> findRepoLikeName(Repo repo) {
+        if (StringUtils.isBlank(repo.getName())) {
+            repo.setName(null);
+        }
+        if (repo.getName() == null) {
+            return repoMapper.findRepoAll();
+        } else {
+            return repoMapper.findRepoLikeName(repo);
+        }
+    }
+
+    @Override
+    public List<Repo> findRepoAll() {
+        return repoMapper.findRepoAll();
     }
 }
