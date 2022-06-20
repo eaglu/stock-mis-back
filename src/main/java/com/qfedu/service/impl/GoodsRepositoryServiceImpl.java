0c6cc5654d3e6400de7ecaf7601b60475247078d
test <test66@game.com>
2022-06-20 17:12:33 +0800

    goodsRepository

--- src/main/java/com/qfedu/service/impl/GoodsRepositoryServiceImpl.java
+++ src/main/java/com/qfedu/service/impl/GoodsRepositoryServiceImpl.java
@@ -5,9 +5,12 @@ import com.qfedu.entity.GoodsRepository;
 import com.qfedu.mapper.GoodsMapper;
 import com.qfedu.mapper.GoodsRepositoryMapper;
 import com.qfedu.service.GoodsRepositoryService;
+import com.qfedu.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,7 +18,8 @@ import java.util.Map;
 public class GoodsRepositoryServiceImpl implements GoodsRepositoryService {
     @Autowired
     private GoodsRepositoryMapper goodsRepositoryMapper;
-    private GoodsMapper goodsMapper;
+    @Autowired
+    private GoodsService goodsService;
 
     @Override
     public Integer insertGoods(GoodsRepository goodsRepository) {
@@ -23,25 +27,30 @@ public class GoodsRepositoryServiceImpl implements GoodsRepositoryService {
         int tempAmount = goodsRepository.getAmount();
         List<GoodsRepository> list = goodsRepositoryMapper.findGoodsRepositoryByRepositoryAndGoods(goodsRepository);
         if (list.size() > 0) {
-            tempAmount=tempAmount+list.get(0).getAmount();
+            tempAmount = tempAmount + list.get(0).getAmount();
             goodsRepository.setAmount(tempAmount);
-            rsCount=goodsRepositoryMapper.updateGoodsRepository(goodsRepository);
-        }else {
-            rsCount=goodsRepositoryMapper.insertGoods(goodsRepository);
+            rsCount = goodsRepositoryMapper.updateGoodsRepository(goodsRepository);
+        } else {
+            rsCount = goodsRepositoryMapper.insertGoods(goodsRepository);
         }
         return rsCount;
     }
 
     @Override
-    public List<Map<Goods,Integer>> findGoodsRepositoryByRepository(GoodsRepository goodsRepository) {
-        List<Map<Goods,Integer>> list = null;
-        Map<Goods,Integer> map = null;
-        Goods goods=null;
-        List<GoodsRepository> goodsRepositoryList=goodsRepositoryMapper.findGoodsRepositoryByRepository(goodsRepository);
-        for (GoodsRepository goodsRepository1 : goodsRepositoryList){
-            goods.setId(goodsRepository1.getGoodsId());
-            Goods goods1=goodsMapper.findGoodsById(goods);
-            map.put(goods1,goodsRepository1.getAmount());
+    public List<Map<Goods, Integer>> findGoodsRepositoryByRepository(GoodsRepository goodsRepository) {
+        List<Map<Goods, Integer>> list = new ArrayList<Map<Goods, Integer>>();
+        Map<Goods, Integer> map = new HashMap<Goods, Integer>();
+
+        List<GoodsRepository> goodsRepositoryList = goodsRepositoryMapper.findGoodsRepositoryByRepository(goodsRepository);
+        for (GoodsRepository goodsRepository1 : goodsRepositoryList) {
+            int id = goodsRepository1.getGoodsId();
+            Goods goods = new Goods();
+            goods.setId(id);
+            Goods goods1 = goodsService.findGoodsById(goods);
+            if (goods1!=null){
+                map.put(goods1, goodsRepository1.getAmount());
+                list.add(map);
+            }
         }
         return list;
     }
